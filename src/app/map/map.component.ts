7fdd45cf29b54fdddc7aff6f41ef15742a927162
Thi Kieu Vi Nguyen <vinguyen1172000@gmail.com>
2021-07-24 23:25:12 -0500

    updated

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,5 +1,3 @@
-//Runette, P (2019). @runette/osm-map-component.ts [source code]. https://gist.github.com/runette/76081e513de5bbc6a1a077d67dd79db5.js
-
 import { Component, OnInit, OnDestroy, Input, Output, EventEmitter } from '@angular/core';
 import { Map, MapOptions, tileLayer, latLng } from 'leaflet';
 
@@ -11,20 +9,20 @@ import { Map, MapOptions, tileLayer, latLng } from 'leaflet';
 export class MapComponent implements OnInit, OnDestroy {
   @Output() map$: EventEmitter<Map> = new EventEmitter;
   @Output() zoom$: EventEmitter<number> = new EventEmitter;
-  @Input() options: MapOptions= {
-                      layers:[tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                        opacity: 0.7,
-                        maxZoom: 19,
-                        detectRetina: true,
-                        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                      })],
-                      zoom:4,
-                      center:latLng(30,-100)
+  @Input() options: MapOptions = {
+    layers: [tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      opacity: 0.7,
+      maxZoom: 19,
+      detectRetina: true,
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    })],
+    zoom: 4,
+    center: latLng(30, -100)
   };
   public map: Map;
   public zoom: number;
 
-  constructor() { 
+  constructor() {
   }
 
   ngOnInit() {
